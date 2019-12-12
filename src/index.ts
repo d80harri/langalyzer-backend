ecb9b24c325566022e872dc89834ee034899df1d
Harald Bittermann (HBI) <harald.bittermann@ntsretail.com>
2019-12-12 07:39:22 +0100

    initial

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import express from "express";
+
+import mongoose, { Document, Schema } from "mongoose";
+
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+// start the Express server
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${port}`);
+});
+
+const uri = "mongodb+srv://root:V26iyeyt.@langalyzer-ysxsi.mongodb.net/test?retryWrites=true&w=majority";
+
+const connect = () => {
+    mongoose
+        .connect(
+            uri,
+            { useNewUrlParser: true }
+        )
+        .then(() => {
+            // tslint:disable-next-line: no-console
+            return console.info(`Successfully connected to ${uri}`);
+        })
+        .catch((error) => {
+            // tslint:disable-next-line: no-console
+            console.error("Error connecting to database: ", error);
+            return process.exit(1);
+        });
+};
+connect();
+
+mongoose.connection.on("disconnected", connect);
+
+export interface ICat extends Document {
+    name: string;
+}
+
+// tslint:disable-next-line: variable-name
+export const CatSchema: Schema<ICat> = new Schema({ name: { type: String } });
+
+export const Cat = mongoose.model<ICat>("Cat", CatSchema);
+
+app.post("/api/document", async (req, res) => {
+    const cat = await new Cat({
+        name: req.body.name
+    }).save();
+
+    return res.send(cat);
+});
+
+app.get("/api/document", async (req, res) => {
+    const result = await Cat.find().exec();
+    return res.send(result);
+});
